c922930f7ac1bd71c9275ced1ac1205f24a52fe5
Matheus Diógenes Amorim <matheusdiogenesdev@gmail.com>
2025-08-29 14:47:31 -0300

    aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./styles/theme.css"
 import "./styles/global.css"
+import { Heading } from "./components/Heading"
 
 export function App() {
     console.log('oi')
     return (
         <>
+            <Heading />
             <h1>Olá, mundo</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam et doloremque minus magnam nam nesciunt libero. Quidem, consequuntur dolores quibusdam nulla eligendi unde culpa, perferendis maiores illo quas asperiores velit! Facilis assumenda corrupti consequatur ea ratione! Modi iusto eius asperiores, explicabo voluptas laudantium, enim illum cum non reiciendis recusandae quasi itaque odio qui earum deserunt doloremque vero provident quos! Commodi earum vel dignissimos magnam, error quod ipsa aperiam! Ratione eligendi ullam ipsa sunt laboriosam iure inventore minima corporis mollitia placeat eveniet sit harum hic, quae quidem in voluptate veniam consectetur dolor, dolore optio. Repellendus id quod quos, similique atque praesentium.</p>
         </>

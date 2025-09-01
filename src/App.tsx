f4fc8a644dfc94600f575524c2efd0a76f1f69dd
Matheus Diógenes Amorim <matheusdiogenesdev@gmail.com>
2025-09-01 15:22:46 -0300

    aula 11

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import "./styles/global.css"
 import { Container } from "./components/Container/Container"
 import { Menu } from "./components/Menu/Menu"
 import { Logo } from "./components/Logo/Logo"
+import { CountDown } from "./components/CountDown/CountDown"
 
 
 export function App() {
     return (
         <>
-
             <Container>
                 <Logo />
             </Container>
 
             <Container>
                 <Menu />
+            </Container>
+
+            <Container>
+                <CountDown />
             </Container>
         </>
     )

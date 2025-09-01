00e731c06918d2c03599045863e729b84e21a59c
Matheus Diógenes Amorim <matheusdiogenesdev@gmail.com>
2025-09-01 11:11:58 -0300

    aula 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./styles/theme.css"
 import "./styles/global.css"
 import { Heading } from "./components/Heading"
+import { TimerIcon } from "lucide-react"
 
 export function App() {
     return (
         <>
-            <Heading attr={123} attr2={"String"}>Olá, mundo</Heading>
+            <Heading>
+                Olá, mundo
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam et doloremque minus magnam nam nesciunt libero. Quidem, consequuntur dolores quibusdam nulla eligendi unde culpa, perferendis maiores illo quas asperiores velit! Facilis assumenda corrupti consequatur ea ratione! Modi iusto eius asperiores, explicabo voluptas laudantium, enim illum cum non reiciendis recusandae quasi itaque odio qui earum deserunt doloremque vero provident quos! Commodi earum vel dignissimos magnam, error quod ipsa aperiam! Ratione eligendi ullam ipsa sunt laboriosam iure inventore minima corporis mollitia placeat eveniet sit harum hic, quae quidem in voluptate veniam consectetur dolor, dolore optio. Repellendus id quod quos, similique atque praesentium.</p>
         </>
     )

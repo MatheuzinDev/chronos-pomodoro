31df7102d9001fe2f1884c4ebf3794b0633332dc
Matheus Diógenes Amorim <matheusdiogenesdev@gmail.com>
2025-09-01 11:59:25 -0300

    aula 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,20 @@
 import "./styles/theme.css"
 import "./styles/global.css"
+import { Container } from "./components/Container/Container"
+import { Heading } from "./components/Heading/Heading"
 
 
 export function App() {
     return (
         <>
 
-            <div className="container">
-                <div className="content">
-                    <section>
-                        LOGO
-                    </section>
-                </div>
-            </div>
+            <Container>
+                <Heading>Logo</Heading>
+            </Container>
 
-            <div className="container">
-                <div className="content">
-                    <section>
-                        MENU
-                    </section>
-                </div>
-            </div>
-
-            <div className="container">
-                <div className="content">
-                    <section>
-                        FORM
-                    </section>
-                </div>
-            </div>
-
-            <div className="container">
-                <div className="content">
-                    <section>
-                        FOOTER
-                    </section>
-                </div>
-            </div>
+            <Container>
+                <Heading>Menu</Heading>
+            </Container>
         </>
     )
 }

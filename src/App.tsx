45f4d7ac60520dad89f044a6249aa77b5d7219d3
Matheus Diógenes Amorim <matheusdiogenesdev@gmail.com>
2025-09-01 11:39:56 -0300

    aula 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
 import "./styles/theme.css"
 import "./styles/global.css"
-import { Heading } from "./components/Heading"
-import { TimerIcon } from "lucide-react"
+
 
 export function App() {
     return (
         <>
-            <Heading>
-                Olá, mundo
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam et doloremque minus magnam nam nesciunt libero. Quidem, consequuntur dolores quibusdam nulla eligendi unde culpa, perferendis maiores illo quas asperiores velit! Facilis assumenda corrupti consequatur ea ratione! Modi iusto eius asperiores, explicabo voluptas laudantium, enim illum cum non reiciendis recusandae quasi itaque odio qui earum deserunt doloremque vero provident quos! Commodi earum vel dignissimos magnam, error quod ipsa aperiam! Ratione eligendi ullam ipsa sunt laboriosam iure inventore minima corporis mollitia placeat eveniet sit harum hic, quae quidem in voluptate veniam consectetur dolor, dolore optio. Repellendus id quod quos, similique atque praesentium.</p>
+
+            <div className="container">
+                <div className="content">
+                    <section>
+                        LOGO
+                    </section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>
+                        MENU
+                    </section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>
+                        FORM
+                    </section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>
+                        FOOTER
+                    </section>
+                </div>
+            </div>
         </>
     )
 }
